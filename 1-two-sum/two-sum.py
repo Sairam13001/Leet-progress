1febafd847b23958c70895d734e7b9ba86e03588
Sairam13001 <70787552+Sairam13001@users.noreply.github.com>
2024-08-02 17:51:07 +0530

    Time: 42 ms (64.05%) | Memory: 13.4 MB (13.64%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -5,7 +5,22 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        for i in range(len(nums)):
-            for j in range(len(nums[i+1:])):
-                if (nums[i]+nums[i+j+1] == target):
-                    return [i,i+j+1]
+        # for i in range(len(nums)):
+        #     for j in range(len(nums[i+1:])):
+        #         if (nums[i]+nums[i+j+1] == target):
+        #             return [i,i+j+1]
+
+        b = list(enumerate(nums))
+        c = sorted(b, key = lambda x:x[1])
+        sorted_nums = [element for index, element in c]
+        sorted_inds = [index for index, element in c]
+        start = 0; end = len(nums) - 1
+        while(True):
+            if (sorted_nums[start] + sorted_nums[end] == target):
+                return [sorted_inds[start], sorted_inds[end]]
+            elif(sorted_nums[start] + sorted_nums[end] < target):
+                start += 1
+            else:
+                end -= 1
+
+        
