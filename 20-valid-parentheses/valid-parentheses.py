c79a64c957aa0833b0616034836601f31664600b
Sairam13001 <70787552+Sairam13001@users.noreply.github.com>
2024-08-04 16:43:28 +0530

    Time: 15 ms (55.43%) | Memory: 11.7 MB (46.40%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        p = {")":"(", "]":"[", "}":"{"}
+        l = [letter for letter in s]
+        if len(l) == 1:
+            return False
+        k = []
+        while(len(l)>0):
+            if len(k) == 0:
+                k.append(l.pop())
+            if (k[-1] in p.keys()) and (len(l)>0):
+                if (p[k[-1]] == l[-1]):
+                    k.pop()
+                    l.pop()
+                else:
+                    k.append(l.pop())
+            else:
+                return False
+        if (len(l) == 0) and (len(k) == 0):
+            return True
+        else:
+            return False
