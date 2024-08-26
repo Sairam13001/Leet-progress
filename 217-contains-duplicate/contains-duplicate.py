8faf919e2c3c1123e7be3e29b5372c1e188a4cc0
Sairam13001 <70787552+Sairam13001@users.noreply.github.com>
2024-08-26 13:32:59 +0530

    Time: 421 ms (76.27%) | Memory: 27.3 MB (62.41%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -4,13 +4,13 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        return len(nums) != len(set(nums))
-        # a = set()
-        # for n in nums:
-        #     if n in a:
-        #         return True
-        #     a.add(n)
-        # return False
+        # return len(nums) != len(set(nums))
+        a = set()
+        for n in nums:
+            if n in a:
+                return True
+            a.add(n)
+        return False
 
             
         
