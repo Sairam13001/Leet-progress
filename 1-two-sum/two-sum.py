2c92fda465ad2d6258d5c26775e308eeb481eea9
Sairam13001 <70787552+Sairam13001@users.noreply.github.com>
2024-08-26 14:03:35 +0530

    Time: 36 ms (82.14%) | Memory: 12.4 MB (73.13%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -10,17 +10,24 @@ class Solution(object):
         #         if (nums[i]+nums[i+j+1] == target):
         #             return [i,i+j+1]
 
-        b = list(enumerate(nums))
-        c = sorted(b, key = lambda x:x[1])
-        sorted_nums = [element for index, element in c]
-        sorted_inds = [index for index, element in c]
-        start = 0; end = len(nums) - 1
-        while(True):
-            if (sorted_nums[start] + sorted_nums[end] == target):
-                return [sorted_inds[start], sorted_inds[end]]
-            elif(sorted_nums[start] + sorted_nums[end] < target):
-                start += 1
-            else:
-                end -= 1
+        # b = list(enumerate(nums))
+        # c = sorted(b, key = lambda x:x[1])
+        # sorted_nums = [element for index, element in c]
+        # sorted_inds = [index for index, element in c]
+        # start = 0; end = len(nums) - 1
+        # while(True):
+        #     if (sorted_nums[start] + sorted_nums[end] == target):
+        #         return [sorted_inds[start], sorted_inds[end]]
+        #     elif(sorted_nums[start] + sorted_nums[end] < target):
+        #         start += 1
+        #     else:
+        #         end -= 1
+
+        map = {}
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in map:
+                return [map[diff], i]
+            map[n] = i
 
         
