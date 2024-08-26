211d7186b35560976de347f74d848a074b020778
Sairam13001 <70787552+Sairam13001@users.noreply.github.com>
2024-08-26 16:03:17 +0530

    Time: 10 ms (88.93%) | Memory: 11.7 MB (51.76%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -4,19 +4,32 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        p = {"(":")", "[":"]", "{":"}"}
-        if len(s)<2:
-            return False
-        a = list(s)
-        b = []
-        while (len(a)>0):
-            if len(b) == 0:
-                b.insert(0, a.pop())
-            elif (a[-1] in p.keys()) and (p[a[-1]] == b[0]):
-                a.pop()
-                b.pop(0)
-            else:
-                b.insert(0, a.pop())
-        return len(b) == 0
+        # p = {"(":")", "[":"]", "{":"}"}
+        # if len(s)<2:
+        #     return False
+        # a = list(s)
+        # b = []
+        # while (len(a)>0):
+        #     if len(b) == 0:
+        #         b.insert(0, a.pop())
+        #     elif (a[-1] in p.keys()) and (p[a[-1]] == b[0]):
+        #         a.pop()
+        #         b.pop(0)
+        #     else:
+        #         b.insert(0, a.pop())
+        # return len(b) == 0
+
+        p = {")":"(", "]":"[", "}":"{"}
+        q = []
+        for c in s:
+            if c not in p:
+                q.append(c)
+                continue
+            if not q or p[c] != q[-1]:
+                return False
+            q.pop()
+        return not q
+
+
 
 
