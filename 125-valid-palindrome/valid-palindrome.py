03a4c2b90b092e0a4f1c9fa1735f7cdf42f10806
Sairam13001 <70787552+Sairam13001@users.noreply.github.com>
2024-08-31 15:21:06 +0530

    Time: 32 ms (53.17%) | Memory: 12.6 MB (73.89%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -4,11 +4,33 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        a = [i.lower() for i in s if i.isalnum()]
-        return a == a[::-1]
+        # a = [i.lower() for i in s if i.isalnum()]
+        # return a == a[::-1]
 
         # new = ''
         # for a in s:
         #     if a.isalnum():
         #         new += a.lower()
         # return (new == new[::-1])
+
+        if not s:
+            return True
+        
+        start = 0
+        last = len(s) - 1
+        
+        while start <= last:
+            currFirst = s[start]
+            currLast = s[last]
+            
+            if not currFirst.isalnum():
+                start += 1
+            elif not currLast.isalnum():
+                last -= 1
+            else:
+                if currFirst.lower() != currLast.lower():
+                    return False
+                start += 1
+                last -= 1
+        
+        return True
